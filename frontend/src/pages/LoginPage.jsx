69ded09149460cea77e772689d0506341aaabe6e
diboramulatu <diboramulatu2021@gmail.com>
2026-06-11 18:14:47 +0300

    Setup frontend with React Router, Tailwind CSS, and API service

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,3 @@
+export default function RestaurantListPage() {
+  return <div>Login Page</div>;
+}
---
+++ frontend/src/pages/RestaurantDetailPage.jsx
@@ -0,0 +1,3 @@
+export default function RestaurantListPage() {
+  return <div>Restaurant Detail Page</div>;
+}
